e9e0627996bb8adc5991531552661e92e84ff456
JaneseoHyeon <49301073+JaneseoHyeon@users.noreply.github.com>
2019-05-26 05:29:29 +0900

    Create _3_신경망.py

---
+++ _3_신경망.py
@@ -0,0 +1,118 @@
+# 계단 함수 그래프
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function(x):
+    return np.array(x>0, dtype=np.int)
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+# 시그모이드 함수 그래프
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+# ReLU 함수
+def relu(x):
+    return np.maximum(0, x)
+
+# 신경망에서의 행렬 곱
+X = np.array([1, 2])
+X.shape
+W = np.array([[1, 3, 5], [2, 4, 6]])
+print(W)
+W.shape
+Y = np.dot(X, W)
+print(Y)
+
+# 3층 신경망 구현
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+print(W1.shape)
+print(X.shape)
+print(B1.shape)
+
+A1 = np.dot(X, W1) + B1
+
+Z1 = sigmoid(A1)
+
+print(A1)
+print(Z1)
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+print(Z1.shape)
+print(W2.shape)
+print(B2.shape)
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+def identity_function(x):
+    return x
+
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1 ,0.2])
+
+A3 = np.dot(Z2, W3) + B3
+Y = identity_function(A3)
+
+# 정리된 3층 신경망 구현
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+
+    return network
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+
+    return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
+
+# 소프트맥스 함수 구현
+a = np.array([0.3, 2.9, 4.0])
+
+exp_a = np.exp(a)
+print(exp_a)
+
+sum_exp_a = np.sum(exp_a)
+print(sum_exp_a)
+
+y = exp_a / sum_exp_a
+print(y)
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
